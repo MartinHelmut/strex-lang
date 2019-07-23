0708076178c0cc79e832e2040edf2fa232fe7a59
Martin Helmut Fieber <martin.fieber@klarna.com>
2019-07-23 14:41:25 +0200

    refactor: some small adjustments to comment texts

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -3,7 +3,7 @@
  *
  * This is the compiler that will evaluate your progress to ensure the resulting
  * AST can also produce real values. Also this enables you to directly test your
- * implementation by producing a value rather an AST.
+ * implementation by producing a result rather an AST.
  */
 "use strict";
 
